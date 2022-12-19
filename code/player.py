19c89933fe156b49fb036554e7f3278eae6a9124
faikaydin <faikcanberkaydin@gmail.com>
2022-12-19 22:57:46 +1100

    sprite groupings, drawing, movement inputs

---
+++ code/player.py
@@ -0,0 +1,64 @@
+import pygame
+from settings import *
+from debug import debug
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, obstacle_sprites):
+        super().__init__(groups)
+        self.image = pygame.image.load('graphics/test/player.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
+        self.obstacle_sprites = obstacle_sprites
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+    def move(self, speed):
+
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        debug(self.direction)
+
+        self.rect.x += self.direction.x * speed
+        self.collision("horizontal")
+        self.rect.y += self.direction.y * speed
+        self.collision("vertical")
+
+    def collision(self, direction):
+        if direction == "horizontal":
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0: # player is moving right
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0: # player is moving left
+                        self.rect.left = sprite.rect.right
+
+        if direction == "vertical":
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0: # player is moving down
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: # player is moving up
+                        self.rect.top = sprite.rect.bottom
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_d]:
+            self.direction.x = 1
+        elif keys[pygame.K_a]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
